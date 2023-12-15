a07e872c25482fc0ce2f5ec3f55875960bd25915
Paul <paul2003mai@yahoo.com>
2023-12-15 06:59:10 -0800

    day2

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Lacatus Paul',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'datePosted': '15.12.2023'
+    },
+    {
+        'author': 'Maier Gia',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'datePosted': '16.12.2023'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
